bd17ebc2e20e86b3c777d1e2b5aaef87a5eee164
jirbyt <jirbyt@yandex.ru>
2020-10-07 18:08:48 +0300

    fix: fix {(}) bug

--- src/index.js
+++ src/index.js
@@ -1,41 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
- let p = 0; 
-
-
-
-let arrStr = [];
-let arr=[];
 let arrBrackets = [];
+let arr =[];
 for(let i =0; i < bracketsConfig.length; i++){
 arrBrackets[i] = arr.concat(bracketsConfig[i]).join('');
 }
-
 arr=arrBrackets.join('').split('');
-arrStr = str.split('')
 
-for(let k = 0; k<arr.length; k++ ) {
-	
-	if(arr[2*k] == arr[2*k+1]) {
+let arrStr = str.split('');
+let strStack ='';
+let flag = false;
 
-		for( let j =0; j<arrStr.length; j++){
-			arr[2*k]===arrStr [j] ? p+=1 :  p+=0;
-		}
-		if (p%2 !==0){return false}
-		else {p=0}
-	}
-	
-	else {
-		
-		for( let j =0; j<arrStr.length; j++){
-			arr[2*k]===arrStr [j] ? p+=1 :  p+=0;
-			arr[2*k+1]===arrStr [j] ? p-=1 :  p+=0;
-			if(p<0){break}
-		}
-  		if (p<0 || p%2 !==0){return false}
-	}
-	
+for(let i = 0; i<arrStr.length; i++ ) {
+for(let j = 0; j<arr.length; j++ ) {
+
+if(arrStr[i]===arr[2*j] && arr[2*j] !==arr[2*j+1]){
+strStack+=arrStr[i]}
+
+else if(arrStr[i]===arr[2*j+1] && arr[2*j] !==arr[2*j+1] && arr[2*j] === strStack.split('')[strStack.length-1]){
+strStack = strStack.slice(0, strStack.length-1)}
+
+else if(arrStr[i]===arr[2*j+1] && arr[2*j] !==arr[2*j+1] && arr[2*j] !== strStack.split('')[strStack.length-1]){
+return false}
+
+
+
+else if(arr[2*j] ===arr[2*j+1] && arrStr[i]===arr[2*j]){
+if (flag === false){
+strStack+=arrStr[i];
+flag=!flag;}
+else if (flag === true){
+if( arr[2*j] === strStack.split('')[strStack.length-1]) {strStack = strStack.slice(0, strStack.length-1);
+flag=!flag}}
+}
+else false
+
+}
 }
 
-return true
+return strStack==="" ? true:false
 }
 
